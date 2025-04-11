bcaf7900c68c6dd6523fb89d1924a3df6b957311
kerhard-153 <kerhard@student.cscc.edu>
2025-04-11 19:48:25 -0400

    Modified create fleet to also create all the rows, changed size of aliens

--- settings.py
+++ settings.py
@@ -41,10 +41,10 @@ class Settings:
         self.ship_h = 64
         self.ship_speed = 5
 
-        self.bullet_file = Path.cwd() / 'Assets' / 'images' / 'green_laser2.png'
-        self.bullet_file_pink = Path.cwd() / 'Assets' / 'images' / 'pink_laser.png'
+        # self.bullet_file = Path.cwd() / 'Assets' / 'images' / 'green_laser2.png'
+        # self.bullet_file_pink = Path.cwd() / 'Assets' / 'images' / 'pink_laser.png'
         self.bullet_file_pb = Path.cwd() / 'Assets' / 'images' / 'p_b_laser.png'
-        self.bullet_file_red = Path.cwd() / 'Assets' / 'images' / 'red_laser.png'
+        # self.bullet_file_red = Path.cwd() / 'Assets' / 'images' / 'red_laser.png'
         self.laser_sound = Path.cwd() / 'Assets' / 'sound' / 'laser7.mp3'
         self.bullet_speed = 7
         self.bullet_w = 24
@@ -52,8 +52,8 @@ class Settings:
         self.bullet_amount = 5
 
         self.alien_file = Path.cwd() / 'Assets' / 'images' / 'alien.png'
-        self.alien_w = 72
-        self.alien_h = 72
+        self.alien_w = 56
+        self.alien_h = 56
         self.fleet_speed = 3
         self.fleet_direction = 1
         self.fleet_drop_speed = 36
