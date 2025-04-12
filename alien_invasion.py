629aaa759df962d9715ac6ac54357f5a6cc309d7
kerhard-153 <kerhard@student.cscc.edu>
2025-04-12 16:21:37 -0400

    Set up ship to reset when colliding with aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -98,6 +98,8 @@ class AlienInvasion:
         pygame.mixer.init()
         self.laser_sound = pygame.mixer.Sound(self.settings.laser_sound)
         self.laser_sound.set_volume(0.5)
+        self.impact_sound = pygame.mixer.Sound(self.settings.impact_sound)
+        self.impact_sound.set_volume(0.2)
 
         self.ship = Ship(self, ShipArsenal(self))
         self.alien_fleet = AlienFleet(self)
@@ -124,8 +126,21 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self.alien_fleet.update_fleet()
+            self._check_collisions()
             self._update_screen()
             self.clock.tick(self.settings.FPS)
+
+    def _check_collisions(self):
+
+        # check collisions for ship
+        if self.ship.check_collisions(self.alien_fleet.fleet):
+            self._reset_level()
+
+
+    def _reset_level(self):
+        self.ship.arsenal.arsenal.empty()
+        self.alien_fleet.fleet.empty()
+        self.alien_fleet.create_fleet()
 
     def _update_screen(self):
 
