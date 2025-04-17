c0832aa2b85ea5340d533f34bbdde37a37f9f6df
kerhard-153 <kerhard@student.cscc.edu>
2025-04-16 20:59:29 -0400

    Set up button to start game and make mouse disappear, made sure no keydown events are captured until button is pressed

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,6 +6,7 @@ from arsenal import ShipArsenal
 from alien_fleet import AlienFleet
 from game_stats import GameStats
 from time import sleep
+from button import Button
 
 class AlienInvasion:
     """
@@ -107,7 +108,8 @@ class AlienInvasion:
         self.ship = Ship(self, ShipArsenal(self))
         self.alien_fleet = AlienFleet(self)
         self.alien_fleet.create_fleet()
-        self.game_active = True
+        self.play_button = Button(self, 'START')
+        self.game_active = False
 
     def run_game(self): 
 
@@ -195,6 +197,13 @@ class AlienInvasion:
         self.alien_fleet.fleet.empty()
         self.alien_fleet.create_fleet()
 
+    def restart_game(self):
+
+        self._reset_level()
+        self.ship._center_ship()
+        self.game_active = True
+        pygame.mouse.set_visible(False)
+
     def _update_screen(self):
 
         """
@@ -213,6 +222,11 @@ class AlienInvasion:
         self.screen.blit(self.bg, (0, 0))
         self.ship.draw()
         self.alien_fleet.draw()
+
+        if not self.game_active:
+            self.play_button.draw()
+            pygame.mouse.set_visible(True)
+
         pygame.display.flip()
 
     def _check_events(self): 
@@ -238,6 +252,13 @@ class AlienInvasion:
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                self._check_button_clicked()
+
+    def _check_button_clicked(self):
+        mouse_pos = pygame.mouse.get_pos()
+        if self.play_button.check_clicked(mouse_pos):
+            self.restart_game()
 
     def _check_keydown_events(self, event):
 
